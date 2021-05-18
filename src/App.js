5edef6e94ff8b6386d1e7a4ef3515435462b91c0
7bel6 <belzeebubc@gmail.com>
2021-05-18 14:50:21 -0400

    Creating Navbar File

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  // we can write any valid javasript here before the return function
+  const title = "Some Text";
+  // ps: we can't use daynimc values inisde react jsx like objects or boolean values
+  
   return (
     <div className="App">
       <div className="content">
-        <h1>App Comp</h1>
+        <h1>{ title }</h1>
       </div>
     </div>
   );
