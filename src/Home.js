603957648b93f3433abca6530260397f4801fa8c
7bel6 <belzeebubc@gmail.com>
2021-05-18 16:20:27 -0400

    showing why we need useState

--- src/Home.js
+++ src/Home.js
@@ -1,12 +1,17 @@
 const Home = () => {
     // Use state in React
+    let name = 'haider';
+    // and let suppose when we click the button we want change the name
+    // here the name will change in the variable but it's not gonna update inside the DOM
     const handleClick = () => {
-        
+        name = 'kenway';
+        console.log('name');
     }
     
     return (
         <div className="home">
             <h2>Hompage</h2>
+            <h2>{ name }</h2>
             <button onClick={handleClick}>Click Me</button>
         </div>
     );
