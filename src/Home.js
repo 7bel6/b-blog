6c934cc2be3ea45c75e8aa66eca93e54b3e4e12a
7bel6 <belzeebubc@gmail.com>
2021-05-18 15:32:52 -0400

    Click handling with passing arguments using anynomus fucntion

--- src/Home.js
+++ src/Home.js
@@ -3,10 +3,20 @@ const Home = () => {
     const handleClick = () => {
         console.log("the button Clicked");
     }
+    // if we want to pass the function argument we can't just call it because the browser will
+    // envoke it dirctly, so we need to create anynomus function
+    const handleClickPass = (name) => {
+        console.log('hello ', name);
+    }
     return (
         <div className="home">
             <h2>Hompage</h2>
             <button onClick={handleClick}>Click Me</button>
+            {/* Passing arg demonstrate */}
+            {/* warp our click function in anynomus functionn */}
+            <button onClick={() => {
+                handleClickPass('Haider')
+            }}>Click with Passing argument</button>
         </div>
     );
 }
