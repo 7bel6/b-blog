924340e84291c4ed17771370ba1925e5ea271f6f
7bel6 <belzeebubc@gmail.com>
2021-05-18 16:24:38 -0400

    explain the reactive value

--- src/Home.js
+++ src/Home.js
@@ -4,6 +4,7 @@ const Home = () => {
     // and let suppose when we click the button we want change the name
     // here the name will change in the variable but it's not gonna update inside the DOM
     const handleClick = () => {
+        // explain of this the value isn't reactive when we click the template doesn't render with the new value, so we have to make the value reactive, so when it's changes react will detect that , and re render the template
         name = 'kenway';
         console.log(name);
     }
