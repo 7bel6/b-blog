e825864740e646e14bb38538b76242b02640d6cc
7bel6 <belzeebubc@gmail.com>
2021-05-18 15:04:57 -0400

    Adding Home Component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import './App.css';
 import Navbar from './Navbar';
+import Home from './Home';
 function App() {
   // we can write any valid javasript here before the return function
-  const title = "Some Text";
   // ps: we can't use daynimc values inisde react jsx like objects or boolean values
 
   return (
     <div className="App">
+      <Navbar />
       <div className="content">
-        <h1>{ title }</h1>
-        <Navbar />
+        <Home />
       </div>
     </div>
   );
