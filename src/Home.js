fd4a377d2ed88f0edbe4e3a7f20fc33829878da7
7bel6 <belzeebubc@gmail.com>
2021-05-18 15:26:16 -0400

    Click handling without arguments

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,12 @@
 const Home = () => {
+    // Now we will create Event handlings
+    const handleClick = () => {
+        console.log("the button Clicked");
+    }
     return (
         <div className="home">
             <h2>Hompage</h2>
+            <button onClick={handleClick}>Click Me</button>
         </div>
     );
 }
