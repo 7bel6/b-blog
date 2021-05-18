14b70548ba2413118d5bb7ba10ca6d8e17c7202f
7bel6 <belzeebubc@gmail.com>
2021-05-18 16:17:11 -0400

    use state prepare

--- src/Home.js
+++ src/Home.js
@@ -1,22 +1,13 @@
 const Home = () => {
-    // Now we will create Event handlings
+    // Use state in React
     const handleClick = () => {
-        console.log("the button Clicked");
+        
     }
-    // if we want to pass the function argument we can't just call it because the browser will
-    // envoke it dirctly, so we need to create anynomus function
-    const handleClickPass = (name) => {
-        console.log('hello ', name);
-    }
+    
     return (
         <div className="home">
             <h2>Hompage</h2>
             <button onClick={handleClick}>Click Me</button>
-            {/* Passing arg demonstrate */}
-            {/* warp our click function in anynomus functionn */}
-            <button onClick={() => {
-                handleClickPass('Haider')
-            }}>Click with Passing argument</button>
         </div>
     );
 }
