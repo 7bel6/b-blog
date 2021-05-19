8406f19bb9444b52e01dcbf9ae426900e168aa51
7bel6 <belzeebubc@gmail.com>
2021-05-19 01:54:06 -0400

    fetching the data

--- src/Home.js
+++ src/Home.js
@@ -2,14 +2,8 @@ import { useState, useEffect } from 'react';
 import Bloglist from './Bloglist';
 // above we importing the hook
 const Home = () => {
-
-    const [blogs, setBlogs] = useState([
-        { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
-        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
-        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
-    ]);
-
-    const [name, setName] = useState('mario');
+    // when we succesfully fetch the data, we gonna update the state with the data
+    const [blogs, setBlogs] = useState(null);
 
     const handleDelete = (id) => {
         const newBlogs = blogs.filter(blog => blog.id !== id);
@@ -17,14 +11,20 @@ const Home = () => {
     }
 
     useEffect(() => {
-        console.log('useEffect on the way');
-    },[name]);
+        // using fetch request here run on intial render
+        fetch('http://localhost:8000/blogs')
+        // when the respones resolve, we get res object this is not the data just a respone object, in order to get the data we have to do something with this object
+            .then(res => {
+                return res.json();
+            })
+            .then(data => {
+                console.log(data)
+            });
+    },[]);
 
     return (
         <div className="home">
-            <Bloglist blogs={blogs} title="all blogs" handleDelete={handleDelete}/>
-            <button onClick={() => setName('haider')}>Change the Name</button>
-            <p>{ name }</p>
+            {/* <Bloglist blogs={blogs} title="all blogs" handleDelete={handleDelete}/> */}
         </div>
     );
 }
