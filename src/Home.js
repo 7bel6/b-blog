0f2ab4988eb4e3e44121b147044e4b4d174d9b8a
7bel6 <belzeebubc@gmail.com>
2021-05-19 01:05:59 -0400

    useEffect Dependencies deomnstarte

--- src/Home.js
+++ src/Home.js
@@ -10,6 +10,9 @@ const Home = () => {
         { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
     ]);
     // we create the function of handle delete here so we pass it as prop and access our hook setBlogs to edit our data which is taking id as an arugment so In blog list we pass the blog id that we want to delete
+
+    //-- demonstarte depandecies on useEffect with simple example
+    const [name, setName] = useState('mario');
     const handleDelete = (id) => {
         // we will use filter method to delete blogs
         // and this actually imitate for deleting data , it's not changing the data just make new array without the filterd data, in case of database like mongodb or firebase we can use methods to delete data really
@@ -25,10 +28,14 @@ const Home = () => {
         // we might use this , for fetch data , or authintication process
         console.log('useEffect on the way');
         // sometimes we don't want this function to run in every render so we pass a depandecy array, [] empty array make the function only run for the intial render, we can add state values that trigger render to change in this array
-    },[]);
+
+        // we add the name as depandency so when the name changed we will run useEffect
+    },[name]);
     return (
         <div className="home">
             <Bloglist blogs={blogs} title="all blogs" handleDelete={handleDelete}/>
+            <button onClick={() => setName('haider')}>Change the Name</button>
+            <p>{ name }</p>
         </div>
     );
 }
