98edf0fb8c85d099c913781cd86bbf7e7a0697cb
Haider <belzeebubc@gmail.com>
2021-05-25 04:35:45 -0400

    Importing Router Components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 
 import Navbar from './Navbar';
 import Home from './Home';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 function App() {
   // we can write any valid javasript here before the return function
   // ps: we can't use daynimc values inisde react jsx like objects or boolean values
