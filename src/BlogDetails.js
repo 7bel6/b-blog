752a6feaac6e158d8ddcc5cf13006a8b84630d5c
Haider <belzeebubc@gmail.com>
2021-05-25 15:28:24 -0400

    Reusing Our Custom Hook

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,15 +1,26 @@
 import { useParams } from "react-router";
-
+import useFetch from './useFetch';
 const BlogDetails = () => {
 
     // Now we want to access whatever the id of blog on url router
     // we put what we named our route paramters in App.js
     const { id } = useParams();
+    // Reusing our custom hook
+    const { data , isPending, error} = useFetch('http://localhost:8000/blogs/' + id);
+
     return(
         <div className='blog-details'>
-            <h2>This is The Blog - { id }</h2>
+            { isPending && <div>Loading...</div>}
+            { error && <div>{ error }</div>}
+            { data && (
+                <article>
+                    <h2>{ data.title }</h2>
+                    <p>Written By : { data.author }</p>
+                    <div>{ data.body }</div>
+                </article>
+            )}
         </div>
     );
 }
 
-export default BlogDetails
+export default BlogDetails;
