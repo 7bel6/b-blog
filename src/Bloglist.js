6b25c1af22282940e8adb7227390bd706307e66b
Haider <belzeebubc@gmail.com>
2021-05-25 08:01:38 -0400

    Bloglist route the link of id

--- src/Bloglist.js
+++ src/Bloglist.js
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 
 const Bloglist = (props) => {
     // we recived the prop here
@@ -5,13 +6,16 @@ const Bloglist = (props) => {
     const title = props.title;
     // we will add delete button to demonstarte passing functions as props like deleting a blog
     return (
+        // we will route the blogs links
         <div className="blog-list">
             {/* we use arrow function inside map method to define the template of outputing these blogs */}
             <h2>{ title }</h2>
             {blogs.map((blog) => (
                 <div className="blog-preview" key={blog.id}>
-                    <h2>{ blog.title }</h2>
-                    <p>Author : { blog.author }</p>
+                    <Link to={`/blogs/${blog.id}`}>
+                        <h2>{ blog.title }</h2>
+                        <p>Author : { blog.author }</p>
+                    </Link>
                 </div>
             ))}
         </div>
