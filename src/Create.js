1f33dacdada1b608bee33e2d37201d254ea53f4d
Haider <belzeebubc@gmail.com>
2021-05-26 05:02:43 -0400

    Controlled inputs demonstration

--- src/Create.js
+++ src/Create.js
@@ -1,4 +1,8 @@
+import { useState } from "react";
+
 const Create = () => {
+    const [title,setTitle] = useState('');
+
     return (
         <div className='create'>
             <h2 className="title">Add new Blog</h2>
@@ -7,6 +11,9 @@ const Create = () => {
                 <input
                     type="text"
                     required
+                    value={title}
+                    // Here we want it when we type it's trigger and change state value
+                    onChange={(e) => setTitle(e.target.value)}
                 />
                 <label>Blog Body : </label>
                 <textarea
@@ -17,6 +24,9 @@ const Create = () => {
                     <option value="mario">mario</option>
                     <option value="luigi">luigi</option>
                 </select>
+                <button>Add Blog</button>
+                {/* Testing the controlled input we show here the change happens on state value */}
+                <p>{ title }</p>
             </form>
         </div>
     );
