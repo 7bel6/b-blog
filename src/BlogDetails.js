8dd5fc7f00ab4340aaaf130ce44c982ef79558e0
Haider <belzeebubc@gmail.com>
2021-05-26 05:45:21 -0400

    Deleting Blog

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,5 +1,6 @@
 import { useParams } from "react-router";
 import useFetch from './useFetch';
+import { useHistory } from 'react-router-dom';
 const BlogDetails = () => {
 
     // Now we want to access whatever the id of blog on url router
@@ -7,6 +8,15 @@ const BlogDetails = () => {
     const { id } = useParams();
     // Reusing our custom hook
     const { data , isPending, error} = useFetch('http://localhost:8000/blogs/' + id);
+    const history = useHistory();
+    const handleClick = () => {
+        fetch('http://localhost:8000/blogs/' + data.id, {
+            method: 'DELETE',
+        })
+        .then(() => {
+            history.push('/')
+        })
+    }
 
     return(
         <div className='blog-details'>
@@ -17,6 +27,8 @@ const BlogDetails = () => {
                     <h2>{ data.title }</h2>
                     <p>Written By : { data.author }</p>
                     <div>{ data.body }</div>
+                    {/* Delete the article */}
+                    <button onClick={handleClick}>Delete</button>
                 </article>
             )}
         </div>
