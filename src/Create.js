caf38ea3a6dd3e655b1a37fdbfafcc848108e2e7
Haider <belzeebubc@gmail.com>
2021-05-26 03:17:17 -0400

    Statrting controlled inputs

--- src/Create.js
+++ src/Create.js
@@ -1,7 +1,23 @@
 const Create = () => {
     return (
         <div className='create'>
-            <h2>Add new Blog</h2>
+            <h2 className="title">Add new Blog</h2>
+            <form>
+                <label>Blog Title : </label>
+                <input
+                    type="text"
+                    required
+                />
+                <label>Blog Body : </label>
+                <textarea
+                    required
+                ></textarea>
+                <label>Blog Author : </label>
+                <select>
+                    <option value="mario">mario</option>
+                    <option value="luigi">luigi</option>
+                </select>
+            </form>
         </div>
     );
 }
